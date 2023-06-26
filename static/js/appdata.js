994f1edbc062d3058563961a86f56a41c0a6d70a
BijoyetaK <bijoyeta.euphoric@gmail.com>
2023-06-25 23:01:54 -0700

    Commit through pycharm dashboard v1

--- static/js/appdata.js
+++ static/js/appdata.js
@@ -1,4 +1,21 @@
+//var genresbyrank = {};
+//var genresbylocation = {};
+//var country_markers = {};
+var marker_array = [];
 
+
+//create basemaps
+var Stamenlayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+   //subdomains: 'abcd',
+   minZoom: 1,
+   maxZoom: 3,
+   ext: 'png'
+});
+var map = L.map('map').setView([36.1408, 5.3536], 2);
+map.addLayer(Stamenlayer);
+
+//canvas - slider - Top 20 songs - See chart.js
 $("canvas[data-year]").each(function(i,data){
     var data_year = $(data).attr("data-year");
     var year_info = Year_metrics[data_year];
@@ -14,9 +31,10 @@ $("canvas[data-year]").each(function(i,data){
             data: {
               labels: data_labels,
               datasets: [{
-                label: 'Songs by Genre',
+                label: 'Number of Songs by Genre',
                 data: data_values,
-                borderWidth: 1
+                borderWidth: 1,
+                backgroundColor:"red"
               }]
             },
             options: {
@@ -26,8 +44,6 @@ $("canvas[data-year]").each(function(i,data){
     }
 
 });
-
-
 
 $("canvas[data-yearinfo]").each(function(i,data){
     var data_year = $(data).attr("data-yearinfo");
@@ -66,42 +82,94 @@ $(".slider").slick({
 
 });
 
-var map_markers = {};
+//var map_markers = {};
 
-var map = L.map('map').setView([36.1408, 5.3536], 1.5);
 
-var Stamenlayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-   //subdomains: 'abcd',
-   minZoom: 1,
-   maxZoom: 2,
-   ext: 'png'
+//for select button - By Genre popularity - Stamen map
+function explorebyGenrePopularity(target_genre)
+{
+    let selected_val = $('#genreselector').val();
+    $('a[data-genre]').hide();
+    $(marker_array).each(function(j,mrk){
+    map.removeLayer(mrk);
+    });
+    $(TopGenres).each(function(i,data)
+    {
+        var genrerank = data['GenreRank'];
+        var ranktext = data["Ranktext"];
+        var ranklocation = data["Location"];
+        if(ranktext==selected_val){
+            $(ranklocation).each(function(j,locationdata){
+                var lat = locationdata["lat"];
+                //lat = lat + j;
+                var lng = locationdata["lng"];
+                var countryiconurl = locationdata["countryiconURL"];
+                var countryname = locationdata["country"]
+                var genre = locationdata["genre"];
+                if(target_genre){
+                    if(target_genre==genre){
+                        var genreicon = locationdata["genreIcon"]
+                        var iconsize = [30,30];
+
+                        var mapIcon = L.icon({
+                            iconUrl: genreicon,
+                            iconSize: iconsize//size of the icon
+                        });
+
+                        var marker = L.marker([lat, lng], {icon: mapIcon});
+                        marker.bindTooltip('<p><img style="padding:2px" src='+countryiconurl+' width="40"  height="30"><b>Country: </b>'+countryname + "</p><hr/><p><b> Genre: </b>"+genre+"</p>");
+                        map.addLayer(marker)
+
+                        marker_array.push(marker);
+                         $('a[data-genre="'+genre+'"]').show();
+                    }
+
+                }
+                else{
+                    var genreicon = locationdata["genreIcon"]
+                    var iconsize = [30,30];
+
+                    var mapIcon = L.icon({
+                        iconUrl: genreicon,
+                        iconSize: iconsize//size of the icon
+                    });
+
+                    var marker = L.marker([lat, lng], {icon: mapIcon});
+                    marker.bindTooltip('<p><img style="padding:2px" src='+countryiconurl+' width="40"  height="30"><b>Country: </b>'+countryname + "</p><hr/><p><b> Genre: </b>"+genre+"</p>");
+                    map.addLayer(marker)
+
+                    marker_array.push(marker);
+                    $('a[data-genre="'+genre+'"]').show();
+
+                }
+
+            })
+
+        }
+
+
+    })
+
+}
+
+function showMarkersByGenre(genre_val){
+   explorebyGenrePopularity(target_genre=genre_val);
+
+}
+//function call for map
+explorebyGenrePopularity(target_genre=null);
+
+//on-click event handling for all buttons for the map
+$("a.genrelink").on("click",function(){
+    let selected_genre = $(this).attr("data-genre");
+    showMarkersByGenre(selected_genre);
+    return false;
 });
 
-map.addLayer(Stamenlayer);
-
-$(mapdata).each(function(i,data){
-var countryname = data['Country'];
-var countrycode = data['CountryCode'];
-var popularGenre = data['Most Popular Genre'];
-var genreIcon = data['GenreIcon'];
-var lat = data['latitude'];
-var lng = data['longitude'];
-var countryurl = "/static/images/flags/"+countrycode+".png"
-console.log(countryurl)
-
-var mapIcon = L.icon({
-    iconUrl: genreIcon,
-    iconSize: [24, 24]
-});
-
-var marker = L.marker([lat, lng], {icon: mapIcon});
-marker.addTo(map);
-marker.bindTooltip('<p><img style="padding:2px" src='+countryurl+' width="40"  height="30"><b>Country: </b>'+countryname + "</p><hr/><p><b>Popular Genre: </b>"+popularGenre+"</p>");
-
-map_markers[popularGenre] = marker;
+//clear filter handling
+$("#reset").on("click",function(){
+    explorebyGenrePopularity(target_genre=null);
+    return false;
 
 })
-
-
 
--- web.py
+++ web.py
@@ -17,22 +17,26 @@ with open(app_config_file,"r") as configobj:
 app = Flask(__name__)
 @app.route('/')
 def welcome():
+    #dt = Spotify_Data.getArtistdata(artist_id='4524518717075603732')
     genredata = Spotify_Data.GenrebyCountry
     genres = Spotify_Data.Genres
-    Top3Genres = Spotify_Data.Top3Genres
+    TopGenres = Spotify_Data.TopGenres
+    UniqueGenres = Spotify_Data.UniqueGenres
+    CountriesbyGenre = Spotify_Data.CountriesbyGenre
 
     headers =['Country','Most Popular Genre','latitude','longitude']
     top20Songs = Spotify_Data.YearlySpotifyData
+
     top20Songs_df = pd.DataFrame(top20Songs)
-    top20Songs_df = top20Songs_df[top20Songs_df["Year"] >= 2000]
+
 
     spotify_top20_by_year = top20Songs_df[top20Songs_df["YearlyPopularityRank"] <= 20]
-    Year_bin = spotify_top20_by_year[["Year", "Title", "Artist", "Genre", "Popularity","GenreIcon","Bpm","Energy","Danceability",
-                                      "Db","Liveness","Valence","Duration","Acousticness","Speechiness"]]
+    Year_bin = spotify_top20_by_year[["Year", "Title", "Artist","Genre", "Popularity","GenreIcon","Bpm","Energy","Danceability",
+                                      "Db","Liveness","Valence","Duration","Acousticness","Speechiness","Artist_ID"]]
     # second bar chart in carousel - which song characteristics that contributed to the popularity in that year
     Year_song_char = pd.DataFrame(Year_bin.groupby("Year").sum())
     #comment
-    Year_song_char.drop(columns=["Title","Artist","Genre","GenreIcon","Popularity","Duration"],inplace=True)
+    Year_song_char.drop(columns=["Title","Artist","Genre","GenreIcon","Popularity","Duration","Artist_ID"],inplace=True)
 
 
     Year_song_char = Year_song_char.to_json(orient="index")
@@ -40,6 +44,7 @@ def welcome():
 
     Year_bin["Song"] = Year_bin.apply(lambda x: dict(Title=x.get("Title"),
                                                      Artist=x.get("Artist"),
+                                                     Artist_ID = x.get("Artist_ID"),
                                                      Genre=x.get("Genre"),
                                                      GenreIcon = x.get("GenreIcon"),
                                                      Popularity=x.get("Popularity"),
@@ -69,7 +74,91 @@ def welcome():
     Year_bin = json.loads(Year_bin)
 
     return render_template("index.html",headers=headers,tableData=genredata, Year_bin=Year_bin,Year_metrics=Year_metrics,
-                           Year_song_char=Year_song_char,genres=genres)
+                           Year_song_char=Year_song_char,genres=genres,TopGenres=TopGenres,
+                           UniqueGenres = UniqueGenres, CountriesbyGenre=CountriesbyGenre)
+
+@app.route('/Timeline')
+def showTimeline():
+    return render_template("timeline.html")
+
+# '/timelineseries' is called from timeline.html
+@app.route('/timelineseries')
+def showTimelineSeries():
+    return render_template("timelineseries.html")
+
+@app.route('/Songs')
+def showSongs():
+    Songs_by_year = Spotify_Data.Songs_by_year
+    Spotify_top_songs = Spotify_Data.Spotify_top_songs
+    Songs = Spotify_Data.Songs
+    Year_by_artists = Spotify_Data.Year_by_artists
+
+    df = pd.DataFrame(Songs_by_year)
+    titles = list(df["Song_Title"])
+    nYears = list(df["Number of Years"])
+    df_songs = pd.DataFrame(Spotify_top_songs)
+    df_songs["Metrics"] = df_songs.apply(lambda x:dict(Year=x.get("Year"),Popularity=x.get("Popularity")),axis=1)
+    df_songs["Artist_metrics"] = df_songs.apply(lambda x:dict(Song=x.get("Title"),Genre=x.get("Genre"),Year=x.get("Year"),
+                                                              Genreicon=x.get("GenreIcon"),Popularity=x.get("Popularity")),axis=1)
+    df_artists = pd.DataFrame(df_songs.groupby("Artist")["Artist_metrics"].apply(list)).reset_index()
+    df_artists = df_artists.to_json(orient="records")
+    Spotify_top_artists = json.loads(df_artists)
+
+
+    df_songs = pd.DataFrame(df_songs.groupby(["Song_Title","Artist","Genre","GenreIcon"])["Metrics"].apply(list)).reset_index()
+    df_songs = df_songs.to_json(orient="records")
+    Spotify_top_songs = json.loads(df_songs)
+
+
+
+    return render_template("songs.html",Songs_by_year=Songs_by_year,titles=titles,nYears=nYears,
+                           Spotify_top_songs=Spotify_top_songs,Spotify_top_artists=Spotify_top_artists,Year_by_artists=Year_by_artists)
+
+@app.route('/Year/<year>')
+def getYearlycharacteristics(year):
+    yearly_data = Spotify_Data.getSongCharacteristics(year=year)
+
+    yearly_data = yearly_data[0]
+
+    popularity = yearly_data.get("Popularity")
+    bpm = yearly_data.get("Bpm")
+    energy = yearly_data.get("Energy")
+    danceability = yearly_data.get("Danceability")
+    liveness = yearly_data.get("Liveness")
+    valence = yearly_data.get("Valence")
+    duration = yearly_data.get("Duration")
+    acousticness = yearly_data.get("Acousticness")
+    speechiness = yearly_data.get("Speechiness")
+
+    bpm_popularity = [[bpm[popularity.index(x)], x] for x in popularity]
+    energy_popularity = [[energy[popularity.index(x)], x] for x in popularity]
+    dance_popularity = [[danceability[popularity.index(x)], x] for x in popularity]
+    live_popularity = [[liveness[popularity.index(x)], x] for x in popularity]
+    valence_popularity = [[valence[popularity.index(x)], x] for x in popularity]
+    duration_popularity = [[duration[popularity.index(x)], x] for x in popularity]
+    acoust_popularity = [[acousticness[popularity.index(x)], x] for x in popularity]
+    speech_popularity = [[speechiness[popularity.index(x)], x] for x in popularity]
+
+    yearly_characteristics_data = {"bpm":bpm_popularity,
+                                 "energy": energy_popularity,
+                                 "danceability": dance_popularity,
+                                 "liveness": live_popularity,
+                                 "valence": valence_popularity,
+                                 "duration": duration_popularity,
+                                 "acousticness": acoust_popularity,
+                                 "speechiness": speech_popularity,
+                                 "year": year
+                                 }
+
+
+    return render_template("characteristics.html",yearly_characteristics_data=yearly_characteristics_data,year=year)
+
+@app.route('/Artist/<artist_id>')
+def getArtiststats(artist_id):
+
+    artist_stats = Spotify_Data.getArtistdata(artist_id=artist_id)
+
+    return render_template("artist.html",artist_stats=artist_stats)
 
 if __name__ == '__main__':
    app.run(port=app_port)
